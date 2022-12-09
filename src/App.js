b21f51b18dc237b4553ef3347643c7e85c7126bb
Mike Oram <Mikeoram01@gmail.com>
2022-12-09 11:45:53 +0000

    Added the container and header-title components and styled them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Container from "./Components/Container/Container";
+import HeaderTitle from "./Components/HeaderTitle/HeaderTitle";
 
 function App() {
     return (
         <div className="App">
+            <Container className={"header-container"}>
+                <HeaderTitle title={"IP Address Tracker"} className={"header-title"}/>
+            </Container>
         </div>
     );
 }
