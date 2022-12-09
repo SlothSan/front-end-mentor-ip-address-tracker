17044a1bf0fe529a61662fa217d19bc4596f62bd
Mike Oram <Mikeoram01@gmail.com>
2022-12-09 13:12:43 +0000

    created the infotext component and styled it, have set up states for all the fields

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import './App.css';
 import Container from "./Components/Container/Container";
 import HeaderTitle from "./Components/HeaderTitle/HeaderTitle";
 import SearchBar from "./Components/SearchBar/SearchBar";
+import {useState} from "react";
+import InfoText from "./Components/InfoText/InfoText";
 
 function App() {
+    const [input, setInput] = useState('');
+    const [ipAddress, setIpAddress] = useState('');
+    const [location, setLocation] = useState('');
+    const [timezone, setTimezone] = useState('');
+    const [isp, setIsp] = useState('')
+
     return (
         <div className="App">
             <Container className={"header-container"}>
                 <HeaderTitle title={"IP Address Tracker"} className={"header-title"}/>
                 <SearchBar/>
+                <Container className={"information-container"}>
+                    <Container className={"infotext-container"}>
+                        <InfoText className={"infotext-header"} text={"IP ADDRESS"}/>
+                        <InfoText className={"infotext"} text={ipAddress}/>
+                    </Container>
+                    <Container className={"infotext-container"}>
+                        <InfoText className={"infotext-header"} text={"LOCATION"}/>
+                        <InfoText className={"infotext"} text={location}/>
+                    </Container>
+                    <Container className={"infotext-container"}>
+                        <InfoText className={"infotext-header"} text={"TIMEZONE"}/>
+                        <InfoText className={"infotext"} text={timezone}/>
+                    </Container>
+                    <Container className={"infotext-container"}>
+                        <InfoText className={"infotext-header"} text={"ISP"}/>
+                        <InfoText className={"infotext"} text={isp}/>
+                    </Container>
+                </Container>
             </Container>
         </div>
     );
